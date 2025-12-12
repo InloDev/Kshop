9a66716bbeb76c06e8faf4eddc8595a36ea72abc
InloDev <vieru.kir@gmail.com>
2025-12-12 11:38:59 +0200

    Add Product, Money, Discount classes

---
+++ Shop/src/Commerce/ProductManagement/Domain/ProductAggregate/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.ProductAggregate;
+
+internal sealed class Product
+{
+    private Guid _id;
+    private string _name;
+    private string _description;
+    private Money _price;
+    private Discount? _discount;
+    private List<Variants> _variants;
+}
+
+internal sealed class Variants
+{
+    private Guid _id;
+    private string _name;
+    private Money _price;
+    private string Sku;
+}
