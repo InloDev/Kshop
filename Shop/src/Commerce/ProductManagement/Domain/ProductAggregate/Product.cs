ab73aba3afe204afd69c0f16765a2ce5d230ee88
InloDev <vieru.kir@gmail.com>
2025-12-16 14:32:22 +0200

    Add methods Create and Update

--- Shop/src/Commerce/ProductManagement/Domain/ProductAggregate/Product.cs
+++ Shop/src/Commerce/ProductManagement/Domain/ProductAggregate/Product.cs
@@ -5,14 +5,59 @@ internal sealed class Product
     private Guid _id;
     private string _name;
     private string _description;
-    private List<Variants> _variants;
-}
+    private List<ProductVariant> _variants =new ();
 
-internal sealed class Variants
-{
-    private Guid _id;
-    private string _name;
-    private Money _price;
-    private Discount? _discount;
-    private string Sku;
+    internal Product(string productName, string description, string variantName, Money price, Discount? discount,
+        string sku)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(productName,"Название продукта не может быть пустым.");
+        ArgumentException.ThrowIfNullOrWhiteSpace(description,"Продукт обязан содержать описание.");
+        _id = Guid.NewGuid();
+        _name = productName;
+        _description = description;
+        AddVariant(variantName, price, discount, sku);
+    }
+
+    internal Product Create(string productName, string description, string variantName, Money price, Discount? discount,
+        string sku)
+    {
+        return new Product(productName, description, variantName, price, discount, sku);
+    }
+
+    internal void Update(string? productName, string? description, string? variantName, Money? price, Discount? discount,
+        string? sku)
+    {
+        if(productName is not null) {_name = productName;}
+        if(description is not null) {_description = description;}
+        if(sku is not null) 
+        {
+            bool isContains = false;
+            foreach (var variant in _variants)
+            {
+                if (variant.Sku == sku)
+                {
+                   if(variantName is not null) variant.Name = variantName;
+                   if(price is not null) variant.Price= price;
+                   if(discount is not null) variant.Discount= discount;
+                    isContains = true;
+                    break;
+                }
+            }
+            if (!isContains) 
+            { 
+                AddVariant(variantName!, price!, discount, sku);
+            }
+        }
+        
+    }
+
+    internal void AddVariant(ProductVariant variant)
+    {
+        _variants.Add(variant);
+    }
+    internal void AddVariant(string name, Money price, Discount? discount,
+        string sku)
+    {
+        _variants.Add(new ProductVariant(name, price, discount, sku));
+    }
 }
---
+++ Shop/src/Commerce/ProductManagement/Domain/ProductAggregate/ProductVariant.cs
@@ -0,0 +1,22 @@
+namespace Domain.ProductAggregate;
+
+internal sealed class ProductVariant
+{
+    internal Guid Id;
+    internal string Name;
+    internal Money Price;
+    internal Discount? Discount;
+    internal readonly string Sku;
+
+    internal ProductVariant (string name, Money price, Discount? discount,
+        string sku)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name,"Название варианта продукта не может быть пустым.");
+        ArgumentException.ThrowIfNullOrWhiteSpace(sku,"Sku код не может быть пустым.");
+        Id = Guid.NewGuid();
+        Name = name;
+        Price = price;
+        Discount = discount;
+        Sku = sku;
+    }
+}
