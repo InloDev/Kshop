a22b6e857385b82f8ec6017a0e0d1ca0c3c81969
InloDev <vieru.kir@gmail.com>
2025-12-23 13:27:03 +0200

    Add RemoveProductCommandHandler

---
+++ Shop/src/Commerce/ProductManagement/Application/RemoveProductCommandHandler.cs
@@ -0,0 +1,21 @@
+namespace KShop.Commerce.ProductManagement.Application;
+
+public class RemoveProductCommandHandler
+{
+    private readonly IProductRepository _productRepository;
+
+    public RemoveProductCommandHandler(IProductRepository productRepository)
+    {
+        ArgumentNullException.ThrowIfNull(productRepository);
+
+        _productRepository = productRepository;
+    }
+
+    public async Task HandleAsync(RemoveProductCommand command, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+
+        var product = await _productRepository.GetAsync(command.ProductId, cancellationToken);
+        await _productRepository.RemoveAsync(product, cancellationToken);
+    }
+}
