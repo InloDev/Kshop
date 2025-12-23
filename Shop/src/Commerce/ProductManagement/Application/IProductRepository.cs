9e8fb4a6168811b68c5248460521fcb741079f8c
InloDev <vieru.kir@gmail.com>
2025-12-23 12:48:55 +0200

    Added methods to ProductRepository

--- Shop/src/Commerce/ProductManagement/Application/IProductRepository.cs
+++ Shop/src/Commerce/ProductManagement/Application/IProductRepository.cs
@@ -4,5 +4,7 @@ namespace KShop.Commerce.ProductManagement.Application;
 
 public interface IProductRepository
 {
+    Task<Product> GetAsync(Guid id);
     Task SaveAsync(Product product, CancellationToken cancellationToken);
+    Task UpdateAsync(Product product, CancellationToken cancellationToken);
 }
