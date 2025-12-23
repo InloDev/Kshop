1832e33d305e3a3e77149667399e7e0a1d3d4d17
InloDev <vieru.kir@gmail.com>
2025-12-23 14:06:56 +0200

    Remove unnecessary Task

--- Shop/src/Commerce/ProductManagement/Application/ProductServices/IProductRepository.cs
+++ Shop/src/Commerce/ProductManagement/Application/ProductServices/IProductRepository.cs
@@ -6,6 +6,5 @@ public interface IProductRepository
 {
     Task<Product> GetAsync(Guid id, CancellationToken cancellationToken);
     Task SaveAsync(Product product, CancellationToken cancellationToken);
-    Task UpdateAsync(Product product, CancellationToken cancellationToken);
     Task RemoveAsync(Product product, CancellationToken cancellationToken);
 }
