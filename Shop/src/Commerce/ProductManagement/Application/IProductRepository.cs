d7d625bf172f9bded14506e6d40bd809cc91c8e2
InloDev <vieru.kir@gmail.com>
2025-12-23 13:01:08 +0200

    Added UpdateProductCommandHandler

--- Shop/src/Commerce/ProductManagement/Application/IProductRepository.cs
+++ Shop/src/Commerce/ProductManagement/Application/IProductRepository.cs
@@ -4,7 +4,7 @@ namespace KShop.Commerce.ProductManagement.Application;
 
 public interface IProductRepository
 {
-    Task<Product> GetAsync(Guid id);
+    Task<Product> GetAsync(Guid id, CancellationToken cancellationToken);
     Task SaveAsync(Product product, CancellationToken cancellationToken);
     Task UpdateAsync(Product product, CancellationToken cancellationToken);
 }
