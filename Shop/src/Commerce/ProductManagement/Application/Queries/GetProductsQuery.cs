06f0c0970c8d25c2790dcda9772ed497008ccd9a
InloDev <vieru.kir@gmail.com>
2026-03-03 14:30:56 +0200

    refactor GetProductsQueryHandler, GetProductsQuery and  simplify code

--- Shop/src/Commerce/ProductManagement/Application/Queries/GetProductsQuery.cs
+++ Shop/src/Commerce/ProductManagement/Application/Queries/GetProductsQuery.cs
@@ -1,6 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace KShop.Commerce.ProductManagement.Application.Queries;
 
 public sealed record GetProductsQuery(
     int PageSize,
-    string? LastName,
-    Guid? LastId);
+    Guid? LastId)
+{
+    public static GetProductsQuery Create(
+        int pageSize,
+        Guid? lastId)
+    {
+        if (pageSize is < 10 or > 100)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return new GetProductsQuery(pageSize,lastId);
+    }
+}
--- Shop/src/Commerce/ProductManagement/Infrastructure/QueryHandlers/GetProductsQueryHandler.cs
+++ Shop/src/Commerce/ProductManagement/Infrastructure/QueryHandlers/GetProductsQueryHandler.cs
@@ -6,32 +6,29 @@ namespace KShop.Commerce.ProductManagement.Infrastructure.QueryHandlers;
 
 public sealed class GetProductsQueryHandler(ProductDbContext dbContext)
 {
-    public async Task<IReadOnlyList<ProductListItemDto>> GetProductListAsync(
+    public Task<IAsyncEnumerable<ProductListItemDto>> GetProductListAsync(
         GetProductsQuery query,
         CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(query);
 
-        var pageSize = Math.Clamp(query.PageSize, 10, 100);
 
-        var productsQuery = dbContext.Set<Product>()
-            .AsNoTracking();
+        IQueryable<Product> productsQuery = dbContext.Set<Product>();
 
-        if (query.LastName is not null && query.LastId is not null)
+        if (query.LastId is not null)
         {
             productsQuery = productsQuery.Where(product =>
-                string.Compare(product.Name.Value, query.LastName, StringComparison.Ordinal) > 0
-                || product.Id > query.LastId);
+                 product.Id > query.LastId);
         }
 
-        var products = await productsQuery
+        var products = productsQuery
             .OrderBy(product => product.Name)
             .ThenBy(product => product.Id)
-            .Take(pageSize)
+            .Take(query.PageSize)
             .Select(product => new ProductListItemDto(product.Id,
                 product.Name.Value,
                 product.Description.Value))
-            .ToListAsync(cancellationToken);
-        return products;
+            .AsAsyncEnumerable();
+        return Task.FromResult(products);
     }
 }
