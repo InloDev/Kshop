67012de2b50b9bf56dc48e2b81796c1d1fc15430
InloDev <vieru.kir@gmail.com>
2026-04-30 09:45:08 +0300

    Configure `JsonStringEnumConverter`, update connection string key, and add Swagger/Endpoints API explorer for improved API functionality

--- Shop/src/Commerce/Startups/Api/Program.cs
+++ Shop/src/Commerce/Startups/Api/Program.cs
@@ -1,15 +1,26 @@
+using System.Text.Json.Serialization;
 using KShop.Commerce.ProductManagement.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("KShopPostgres");
 
 builder.Services.AddProductManagement(connectionString!);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    })
+    ;
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
