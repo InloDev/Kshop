712d5a7d83583dc6b54dd70354d7b8f7525aa45a
InloDev <vieru.kir@gmail.com>
2026-06-12 23:03:03 +0300

    Remove OrderDto and OrderItemDto from Application layer

--- Shop/src/Commerce/OrderManagement/Application/DTO/OrderItemDto.cs
+++
@@ -1,8 +0,0 @@
-namespace KShop.Commerce.OrderManagement.Application.DTO;
-
-public sealed record OrderItemDto(
-    Guid ProductId,
-    string ProductName,
-    int Quantity,
-    decimal UnitPrice,
-    decimal Discount);
